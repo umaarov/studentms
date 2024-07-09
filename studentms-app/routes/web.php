1baf157a8eccaca97342d1684161c8d2483a1381
Ismoiljon <hs.umarov21@gmail.com>
2024-07-09 13:42:12 +0500

    teacher crud

--- studentms-app/routes/web.php
+++ studentms-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::resource('/students', StudentController::class);
+Route::resource('/teachers', TeacherController::class);
