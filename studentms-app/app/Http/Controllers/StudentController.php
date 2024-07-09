6772af103e5408233a1619b766cf5ad48c824968
Ismoiljon <hs.umarov21@gmail.com>
2024-07-09 11:47:06 +0500

    student delete

--- studentms-app/app/Http/Controllers/StudentController.php
+++ studentms-app/app/Http/Controllers/StudentController.php
@@ -10,26 +10,17 @@ use Illuminate\View\View;
 
 class StudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index(): View
     {
         $students = Student::all();
         return view ('students.index')->with('students', $students);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create(): View
     {
         return view('students.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request): RedirectResponse
     {
         $input = $request->all();
@@ -37,27 +28,18 @@ class StudentController extends Controller
         return redirect('students')->with('flash_message', 'Student Added!');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id): View
     {
         $student = Student::find($id);
         return view('students.show')->with('students', $student);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id): View
     {
         $student = Student::find($id);
         return view('students.edit')->with('students', $student);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id): RedirectResponse
     {
         $student = Student::find($id);
@@ -66,11 +48,9 @@ class StudentController extends Controller
         return redirect('students')->with('flash_message', 'Student Updated !');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(string $id): RedirectResponse
     {
-        //
+        Student::destroy($id);
+        return redirect('students')->with('flash_message', 'Student deleted!');
     }
 }
